0d1faca02769b3c0dc388cf2aa35a5763f76c1b3
hayyim0626 <h19960626@gmail.com>
2024-11-13 22:36:48 +0900

    [test]: jest config 설정 변경 및 setup.ts 파일 추가. tsconfig.json 파일 "jsx" 수정(preserve => react-jsx)

--- jest.config.ts
+++ jest.config.ts
@@ -1,16 +1,16 @@
 import type { Config } from "jest";
-import nextJest from "next/jest";
-
-const createJestConfig = nextJest({
-  dir: "./"
-});
 
 const config: Config = {
-  coverageProvider: "v8",
   testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
   moduleNameMapper: {
-    "^@/components/(.*)$": "<rootDir>/components/$1"
+    "^@/components/(.*)$": "<rootDir>/components/$1",
+    "^@/pages/(.*)$": "<rootDir>/src/pages/$1",
+    "^@/utils/(.*)$": "<rootDir>/utils/$1"
+  },
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest"
   }
 };
 
-export default createJestConfig(config);
+export default config;
