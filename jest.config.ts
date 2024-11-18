e315193e6fb1fb783191f9aa62fa5bd26719b618
hayyim0626 <h19960626@gmail.com>
2024-11-18 21:13:19 +0900

    [test]: jest config 파일 수정, package.json 타입 테스트 추가

--- jest.config.ts
+++ jest.config.ts
@@ -8,8 +8,20 @@ const config: Config = {
     "^@/utils/(.*)$": "<rootDir>/utils/$1"
   },
   transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest"
-  }
+    "^.+\\.(ts|tsx)$": [
+      "ts-jest",
+      {
+        tsconfig: "tsconfig.test.json",
+        isolatedModules: false, // 전체 프로젝트 타입 체크 활성화
+        diagnostics: {
+          warnOnly: false // 타입 에러를 경고가 아닌 에러로 처리
+        }
+      }
+    ]
+  },
+  testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/.next/"],
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  rootDir: "."
 };
 
 export default config;
